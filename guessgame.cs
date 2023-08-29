72b3f00a04826f3df19ca2c5bd24e1f0adff8bbe
anita-molnar <142496228+anita-molnar@users.noreply.github.com>
2023-08-29 16:37:08 +0100

    Update guessgame.cs

--- guessgame.cs
+++ guessgame.cs
@@ -10,9 +10,6 @@ namespace MyProject
 {
     internal class Program
     {
-
-
-
         static void Main(string[] args)
         {
             Console.Write("Write a northern italian city in lower case.\n");
@@ -52,20 +49,14 @@ namespace MyProject
             // checking if user out of guesses 
             if (outOfGuesses)
             {
-                Console.Write("You lose! You had " + attempts + " attempts. The word was " + secretWord );
+                Console.Write("You lose! You had " + attempts + " attempts. The word was " + secretWord + "." );
             }
             else
             {
                 Console.Write("You win!");
             }
-
-
-
             Console.ReadLine();
         }
-
-
     }
-
 }
 
